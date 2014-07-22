adb0936029a3ba425ff8222bbbaa512f2877048d
Alvise Dorigo <alvise72@gmail.com>
2014-07-22 11:29:47 +0200

    wip

--- src/org/openstack/views/UserView.java
+++ src/org/openstack/views/UserView.java
@@ -22,13 +22,14 @@ public class UserView extends LinearLayout {
 
     private Context ctx = null;
 
-    private LinearLayoutNamed row           = null;
-    private LinearLayoutNamed buttonsLayout = null;
-    private LinearLayoutNamed userLayout    = null;
-    private TextViewNamed     textUserName  = null;
-    private TextViewNamed     textEndpoint  = null;
-    private ImageButtonNamed  modifyUser    = null;
-    private ImageButtonNamed  deleteUser    = null;
+    private LinearLayoutNamed row            = null;
+    private LinearLayoutNamed buttonsLayout  = null;
+    private LinearLayoutNamed userLayout     = null;
+    private TextViewNamed     textUserName   = null;
+    private TextViewNamed     textTenantName = null;
+    private TextViewNamed     textEndpoint   = null;
+    // private ImageButtonNamed  modifyUser     = null;
+    private ImageButtonNamed  deleteUser     = null;
     
     private User user = null;
 
@@ -43,33 +44,42 @@ public class UserView extends LinearLayout {
 	setLayoutParams( params1 );
 	int padding = Utils.getDisplayPixel( ctx, 2 );
 	setPadding( padding, padding, padding, padding );
-	//	setBackgroundResource(R.drawable.rounded_corner_thin);
 
 	row = new LinearLayoutNamed( ctx, this );
 	row.setOrientation( LinearLayout.HORIZONTAL );
 	LinearLayout.LayoutParams _params1
-	    = new LinearLayout.LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+	    = new LinearLayout.LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 	row.setLayoutParams( _params1 );
 	row.setBackgroundResource(R.drawable.rounded_corner_thin);
 
 	userLayout = new LinearLayoutNamed( ctx, this );
 	userLayout.setOrientation( LinearLayout.VERTICAL );
 	LinearLayout.LayoutParams params2 
-	    = new LinearLayout.LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
+	    = new LinearLayout.LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 	userLayout.setLayoutParams( params2 );
 	
 	textUserName = new TextViewNamed( ctx, (UserView)this );
-	textUserName.setText(user.getUserName()+" ("+user.getTenantName()+")");
+	textUserName.setText("User: "+user.getUserName() );
 	textUserName.setTextColor( Color.parseColor("#333333") );
 	textUserName.setOnClickListener( (OnClickListener)ctx );
+	textUserName.setTextColor( Color.parseColor("#BBBBBB"));
+
+	textTenantName = new TextViewNamed( ctx, (UserView)this );
+	textTenantName.setText("Tenant: "+user.getTenantName() );
+	textTenantName.setTextColor( Color.parseColor("#333333") );
+	textTenantName.setOnClickListener( (OnClickListener)ctx );
+	textTenantName.setTextColor( Color.parseColor("#BBBBBB"));
+
 	textEndpoint = new TextViewNamed( ctx, (UserView)this );
-	textEndpoint.setText(U.getEndpoint( ));
+	textEndpoint.setText("Endpoint: "+U.getEndpoint( ));
 	textEndpoint.setTextColor( Color.parseColor("#333333") );
 	textEndpoint.setOnClickListener( (OnClickListener)ctx );
 	textEndpoint.setTextColor( Color.parseColor("#BBBBBB"));
-	textUserName.setTextColor( Color.parseColor("#BBBBBB"));
+
+	
 
 	userLayout.addView(textUserName);
+	userLayout.addView(textTenantName);
 	userLayout.addView(textEndpoint);
 	userLayout.setOnClickListener( (OnClickListener)ctx );
 
@@ -78,20 +88,20 @@ public class UserView extends LinearLayout {
       
 	buttonsLayout = new LinearLayoutNamed( ctx, this );
 	buttonsLayout.setOrientation( LinearLayout.HORIZONTAL );
-	LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams( LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT );
+	LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT );
 	params3.gravity=Gravity.RIGHT;
 	buttonsLayout.setLayoutParams( params3 );
-	buttonsLayout.setGravity( Gravity.RIGHT );
+	buttonsLayout.setGravity( Gravity.RIGHT|Gravity.CENTER_VERTICAL );
 	
-	modifyUser = new ImageButtonNamed( ctx, this, ImageButtonNamed.BUTTON_MODIFY_USER );
-	modifyUser.setImageResource(android.R.drawable.ic_menu_edit);
-	modifyUser.setOnClickListener( (OnClickListener)ctx );
+// 	modifyUser = new ImageButtonNamed( ctx, this, ImageButtonNamed.BUTTON_MODIFY_USER );
+// 	modifyUser.setImageResource(android.R.drawable.ic_menu_edit);
+// 	modifyUser.setOnClickListener( (OnClickListener)ctx );
 	
 	deleteUser = new ImageButtonNamed( ctx, this, ImageButtonNamed.BUTTON_DELETE_USER );
 	deleteUser.setImageResource(android.R.drawable.ic_menu_delete);
 	deleteUser.setOnClickListener( (OnClickListener)ctx );
 	
-	buttonsLayout.addView( modifyUser );
+	//	buttonsLayout.addView( modifyUser );
 	buttonsLayout.addView( deleteUser );
 	buttonsLayout.setOnClickListener( (OnClickListener)ctx );
 	
@@ -102,15 +112,19 @@ public class UserView extends LinearLayout {
     public void setSelected( ) {
 	textEndpoint.setTypeface( null, Typeface.BOLD );
 	textUserName.setTypeface( null, Typeface.BOLD );
+	textTenantName.setTypeface( null, Typeface.BOLD );
 	textEndpoint.setTextColor( Color.parseColor("#00AA00") );
 	textUserName.setTextColor( Color.parseColor("#00AA00") );
+	textTenantName.setTextColor( Color.parseColor("#00AA00") );
     }
 
     public void setUnselected( ) {
 	textEndpoint.setTypeface( null, Typeface.NORMAL );
 	textUserName.setTypeface( null, Typeface.NORMAL );
+	textTenantName.setTypeface( null, Typeface.NORMAL );
 	textEndpoint.setTextColor( Color.parseColor("#BBBBBB") );
 	textUserName.setTextColor( Color.parseColor("#BBBBBB") );
+	textTenantName.setTextColor( Color.parseColor("#BBBBBB") );
     }
 
     public String getFilename( ) { return user.getUserID()+"."+user.getTenantID(); }
