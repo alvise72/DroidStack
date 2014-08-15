4c2aa2a8f3c70d3b4780e4162d93fd50d2526730
Alvise Dorigo <alvise72@gmail.com>
2014-08-15 10:57:47 +0200

    wip

---
+++ src/org/openstack/activities/EditSecGroupActivity.java
@@ -0,0 +1,20 @@
+package org.openstack.activities;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class EditSecGroupActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.editsecgroup );
+
+    }
+
+    public void addRuleE( View v ){}
+    public void addRuleI( View v ){}
+
+}
