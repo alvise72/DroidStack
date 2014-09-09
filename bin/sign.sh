0083046aeb6389ce2360e8fe62167fa8ae117a38
Alvise Dorigo <alvise72@gmail.com>
2014-09-09 15:28:34 +0200

    added script to sign apk

---
+++ bin/sign.sh
@@ -0,0 +1,6 @@
+$JAVA_HOME/bin/keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+ant release
+$JAVA_HOME/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore bin/DroidStack-release-unsigned.apk alias_name
+mv bin/DroidStack-release-unsigned.apk bin/DroidStack.apk
+$JAVA_HOME/bin/jarsigner -verify -verbose -certs bin/DroidStack.apk
+
