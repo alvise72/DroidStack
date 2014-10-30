7c2c4dbb0192ee9526481fd896e2a5a634082eec
Alvise Dorigo <alvise72@gmail.com>
2014-10-30 21:49:33 +0100

    work in progress on error handling restructuring

--- src/org/stackdroid/comm/ServerErrorException.java
+++
@@ -1,8 +0,0 @@
-package org.stackdroid.comm;
-
-public class ServerErrorException extends Exception {
-	private static final long serialVersionUID = 1087368867376448461L;
-    public ServerErrorException(String message) {
-	super(message);
-    }
-}
