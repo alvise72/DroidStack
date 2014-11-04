b53c5025fcecf67bb60d93b7920f0884fce138da
Alvise Dorigo <alvise72@gmail.com>
2014-11-04 23:20:14 +0100

    new source file

---
+++ src/org/stackdroid/utils/CheckBoxWithView.java
@@ -0,0 +1,102 @@
+package org.stackdroid.utils;
+
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.content.Context;
+
+import org.stackdroid.views.FloatingIPView;
+import org.stackdroid.views.ListSecGroupView;
+import org.stackdroid.views.NetworkListView;
+import org.stackdroid.views.RuleView;
+import org.stackdroid.views.SecGroupView;
+import org.stackdroid.views.UserView;
+import org.stackdroid.views.ServerView;
+import org.stackdroid.views.OSImageView;
+import org.stackdroid.views.NetworkView;
+import org.stackdroid.views.VolumeView;
+
+public class CheckBoxWithView extends CheckBox implements GetView {
+
+	private RuleView relatedRuleView = null;
+    private UserView relatedUserView = null;
+    private ServerView relatedServerView = null;
+    private VolumeView relatedVolumeView = null;
+    private OSImageView relatedOSImageView = null;
+    private NetworkView relatedNetworkView = null;
+    private SecGroupView relatedSecGroupView = null;
+    private FloatingIPView relatedFloatingIPView = null;
+    private NetworkListView relatedNetworkListView = null;
+    private ListSecGroupView relatedListSecGroupView = null;
+    
+    public CheckBoxWithView( Context ctx, UserView uv ) {
+    	super( ctx );
+    	relatedUserView = uv;
+    }
+
+    public CheckBoxWithView( Context ctx, ServerView sv ) {
+    	super( ctx );
+    	relatedServerView = sv;
+    }
+
+    public CheckBoxWithView( Context ctx, OSImageView sv ) {
+    	super( ctx );
+    	relatedOSImageView = sv;
+    }
+
+    public CheckBoxWithView( Context ctx, NetworkView nv ) {
+    	super( ctx );
+    	relatedNetworkView = nv;
+    }
+    
+    public CheckBoxWithView( Context ctx, ListSecGroupView nv ) {
+    	super( ctx );
+    	relatedListSecGroupView = nv;
+    }
+
+    public CheckBoxWithView( Context ctx, VolumeView vv ) {
+   	    super( ctx );
+   	    relatedVolumeView = vv;
+    }
+    
+    public CheckBoxWithView( Context ctx, RuleView rv ) {
+   	    super( ctx );
+   	    relatedRuleView = rv;
+    }
+    
+    public CheckBoxWithView(Context ctx, NetworkListView nv ){
+    	super( ctx );
+   	    relatedNetworkListView = nv;
+    }
+    
+    public CheckBoxWithView(Context ctx, SecGroupView sv ){
+    	super( ctx );
+    	relatedSecGroupView = sv;
+    }
+    
+    public CheckBoxWithView(Context ctx, FloatingIPView nv ){
+    	super( ctx );
+    	relatedFloatingIPView = nv;
+    }
+    
+    
+    @Override
+	public RuleView getRuleView( ) { return relatedRuleView; }
+    @Override
+	public UserView getUserView( ) { return relatedUserView; }
+    @Override
+	public ServerView getServerView( ) { return relatedServerView; }
+    @Override
+	public OSImageView getOSImageView( ) { return relatedOSImageView; }
+    @Override
+	public NetworkView getNetworkView( ) { return relatedNetworkView; }
+    @Override
+	public ListSecGroupView getListSecGroupView( ) { return relatedListSecGroupView;}
+    @Override
+	public FloatingIPView getFloatingIPView( ) { return relatedFloatingIPView; }
+    @Override
+	public VolumeView getVolumeView( ) { return relatedVolumeView; }
+    @Override
+	public SecGroupView getSecGroupView( ) { return relatedSecGroupView; }
+    @Override
+	public NetworkListView getNetworkListView() { return relatedNetworkListView; }
+};
